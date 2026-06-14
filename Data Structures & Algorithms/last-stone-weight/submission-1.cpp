70e24c4fa5629e000385703568b4c10ea8812df3
Nitant Panicker <97420952+nitant-p@users.noreply.github.com>
2026-06-14 21:37:05 +0800

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> heap(stones.begin(), stones.end());
+
+        while (heap.size() > 1) {
+            int heaviest = heap.top();
+            heap.pop();
+
+            int sHeaviest = heap.top();
+            heap.pop();
+
+            if (heaviest > sHeaviest) {
+                heaviest -= sHeaviest;
+                heap.push(heaviest);
+            }
+        }
+
+        return heap.size() == 1 ? heap.top() : 0;
+    }
+};
