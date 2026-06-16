a7e5cfabb91b41d1bc069e1e4b23a68a1b1f9faf
Nitant Panicker <97420952+nitant-p@users.noreply.github.com>
2026-06-16 22:30:04 +0800

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<tuple<double, int>> heap;
+        for (auto i = 0; i < points.size(); ++i) {
+            tuple<double, int> entry = {calcDistance(points[i][0], points[i][1]), i};
+            heap.push(entry);
+            if (heap.size() > k) heap.pop();
+        }
+
+        // while (heap.size() > k) heap.pop();
+        
+        vector<vector<int>> res;
+        while (!heap.empty()) {
+            // cout <<"lol"<<endl;
+            // cout<< get<1>(heap.top());
+            res.push_back(points[get<1>(heap.top())]);
+            heap.pop();
+        }
+        return res;
+     }
+
+    double calcDistance(int x, int y) {
+        return sqrt(pow(x, 2) + pow(y, 2));
+    }
+};
