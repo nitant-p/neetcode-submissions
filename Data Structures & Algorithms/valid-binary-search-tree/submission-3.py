2d65e5d57d1ef29d09bb19eb0d10ec5ae0f060ef
Nitant Panicker <97420952+nitant-p@users.noreply.github.com>
2026-09-19 13:57:44 +0800

    Add: valid-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-3.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node, maxx, minn):
+            if not node:
+                return True
+            else:
+                if minn < node.val < maxx:
+                    return dfs(node.left, node.val, minn) and dfs(node.right, maxx, node.val)
+                else:
+                    return False
+
+
+
+        return dfs(root, float('inf'), float('-inf'))
